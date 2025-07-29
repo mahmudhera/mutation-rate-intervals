51c8247f43f0432be59a9836314e53559ea90daa
Mahmudur <mahmudhera93@gmail.com>
2025-07-29 03:03:00 -0400

    investigate the width of intervals

---
+++ mrh_test_width_of_interval.py
@@ -0,0 +1,16 @@
+from kmer_mutation_formulas_thm5 import confidence_interval_r1_from_n_mutated
+import math
+
+L = 10000
+k = 41
+alpha = 0.05
+
+for x in range(101):
+    r1 = x/100.0
+    c0, c1 = confidence_interval_r1_from_n_mutated(L,k,r1,alpha)
+    if math.isnan(c0):
+        c0 = 0.0
+    if math.isnan(c1):
+        c1 = 1.0
+        
+    print(f"{r1:.2f} {c1-c0:.4f}")
